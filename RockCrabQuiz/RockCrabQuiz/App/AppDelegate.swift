68e9c640dd10412a564bc04cb9874f5d47717877
YuSeongChoi <marine8743@naver.com>
2025-01-24 16:09:32 +0900

    add: Firebase Library 추가

--- RockCrabQuiz/RockCrabQuiz/App/AppDelegate.swift
+++ RockCrabQuiz/RockCrabQuiz/App/AppDelegate.swift
@@ -6,11 +6,10 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @MainActor
 final class AppDelegate: NSObject, UIApplicationDelegate, ObservableObject, Identifiable {
-    
-    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         let configuration = connectingSceneSession.configuration.copy() as! UISceneConfiguration
         switch connectingSceneSession.role {
@@ -22,5 +21,10 @@ final class AppDelegate: NSObject, UIApplicationDelegate, ObservableObject, Iden
             break
         }
         return configuration
+    }
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
+        return true
     }
 }
